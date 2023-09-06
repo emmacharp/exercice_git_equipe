e61c7a9374084c7319f21c4e1455a904ea35d011
fhmaisonneuve <e2296133@cmaisonnuve.qc.ca>
2023-09-06 14:32:41 -0400

    solved merge conflict

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 // Auteur : Francois Hébert
 console.log("test")
+=======
+
+>>>>>>> 12e47e1 (résolution de conflit)
 // bla bla
 1=1;
+
+// Auteur : Francois Hébert
+console.log("test")
 
